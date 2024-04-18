3d3dd2d1782e20ba424c85d8427b5f471cece257
JFarah <joaquinefarah@gmail.com>
2024-04-18 13:31:32 -0300

    añadidas imagenes de autos

--- components/CardCard.tsx
+++ components/CardCard.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import CustomButton from './CustomButton';
 import { CarProps } from "@/types";
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageURL } from '@/utils';
 import CarDetails from './CardDetails';
 
 
@@ -36,7 +36,7 @@ const CardCard = ({ car }: CarCardProps) => {
         </span>
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
-        <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+        <Image src={generateCarImageURL(car, '29')} alt="car model" fill priority className="object-contain" />
       </div>
       <div className="relative flex w-full mt-2">
         <div className="flex group-hover:invisible w-full justify-between text-gray">
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers =  {
 		'X-RapidAPI-Key': '5d0a18c70fmsh91a43ffb289f473p1689bfjsnc32b654aaa04',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
         headers: headers,
     });
 
@@ -26,5 +28,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   
     return rentalRatePerDay.toFixed(0);
-  };
+};
+
+export const generateCarImageURL = (car: CarProps, angle?: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const { make, year, model } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+}
   
